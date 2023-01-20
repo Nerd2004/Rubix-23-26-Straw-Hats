c0673ca645c986bc76bd88f250b7f709045298c9
Nerd <sssagarbro@gmail.com>
2023-01-20 12:15:59 +0530

    LAST COMMIT

--- js/lobby.js
+++ js/lobby.js
@@ -16,3 +16,62 @@ form.addEventListener('submit', (e) => {
     }
     window.location = `room.html?room=${inviteCode}`
 })
+
+
+//FRONT END CODE
+//sidebar
+const menuItems = document.querySelectorAll('.menu-item');
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+// const message = messages.querySelectorAll('.message');
+// const messageSearch = document.querySelector('#message-search');
+// let x=document.getElementsByClassName('notifications-popup');
+// console.log(x);
+//remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+let x =document.getElementById('popupaaya');
+menuItems.forEach(item => {
+    item.onclick = () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id== 'community') {
+            document.getElementById('popupaaya').style.visibility="visible";
+            document.getElementById('notifications-popup').style.visibility="hidden";
+            document.getElementById('form__container').style.visibility="hidden";
+        }
+        if(item.id== 'notifications') {
+            document.getElementById('notifications-popup').style.visibility="visible";
+            document.getElementById('form__container').style.visibility="hidden";
+            // document.getElementById('popupaaya').style.visibility="hidden";
+        }
+        else if(item.id=='messages-notifications'){
+            window.open('https://demochatapp.onrender.com/', '_self');
+        }
+        else if(item.id =='videochat'){
+            document.getElementById('form__container').style.visibility="visible";
+            document.getElementById('notifications-popup').style.visibility="hidden";
+            // document.getElementById('popupaaya').style.visibility="hidden";
+        }
+
+         else{
+            document.getElementById('form__container').style.visibility="hidden";
+        }
+    }
+})
+
+
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name=chat.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        } else{
+            chat.style.display = 'none';
+        }
+    })
+}
